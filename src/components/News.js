afe73afaaf549dc9bc9478e907e34d467a0947b3
NIlanjan-Adhikari <nilanjan.adhikari.phe17@itbhu.ac.in>
2023-11-18 21:14:01 +0530

    Fetch API, next and previous buttons added

--- src/components/News.js
+++ src/components/News.js
@@ -2,20 +2,90 @@ import React, { Component } from "react";
 import NewsItem from "./NewsItem";
 
 export class News extends Component {
+  constructor() {
+    super();
+    console.log("This is from constructor in news components.");
+    this.state = {
+      articles: [],
+      loading: false,
+      page: 1,
+    };
+  }
+
+  async componentDidMount() {
+    let url =
+      "https://newsapi.org/v2/top-headlines?country=in&apiKey=2f50ab40cb524053bf86d0b1f9be9d71&page=1&pageSize=20";
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    console.log(parsedData);
+    this.setState({ articles: parsedData.articles, totalResults: parsedData.totalResults });
+  }
+
+  handlePrevClick = async () => {
+    console.log("Previous");
+    let url =
+      `https://newsapi.org/v2/top-headlines?country=in&apiKey=2f50ab40cb524053bf86d0b1f9be9d71&page=${this.state.page-1}&pageSize=20`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    console.log(parsedData);
+    this.setState({
+      page: this.state.page + 1, 
+      articles: parsedData.articles 
+    });
+  };
+
+  handleNextClick = async () => {
+    console.log("Next");
+    if(this.state.page + 1 > Math.ceil(this.state.totalResults/20)){
+
+    }
+    else{
+    let url =
+      `https://newsapi.org/v2/top-headlines?country=in&apiKey=2f50ab40cb524053bf86d0b1f9be9d71&page=${this.state.page+1}&pageSize=20`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    console.log(parsedData);
+    this.setState({
+      page: this.state.page + 1, 
+      articles: parsedData.articles 
+    });
+  }
+  };
+
   render() {
     return (
       <div className="container my-3">
         <h2>NewsMonkey - Top Headlines</h2>
         <div className="row">
-          <div className="col-md-4">
-            <NewsItem title="mytitle" description="mydesc" />
-          </div>
-          <div className="col-md-4">
-            <NewsItem title="mytitle" description="mydesc" />
-          </div>
-          <div className="col-md-4">
-            <NewsItem title="mytitle" description="mydesc" />
-          </div>
+          {this.state.articles.map((element) => {
+            return (
+              <div className="col-md-4" key={element.url}>
+                <NewsItem
+                  title={element.title ? element.title : ""}
+                  description={element.description ? element.description : ""}
+                  imageUrl={element.urlToImage}
+                  newsUrl={element.url}
+                />
+              </div>
+            );
+          })}
+        </div>
+        <div className="container d-flex justify-content-between">
+          <button
+            type="button"
+            className="btn btn-dark"
+            onClick={this.handlePrevClick}
+            disabled={this.state.page<=1}
+          >
+            &larr; Previous
+          </button>
+          <button
+            type="button"
+            className="btn btn-dark"
+            onClick={this.handleNextClick}
+          >
+            Next &rarr;
+          </button>
         </div>
       </div>
     );
