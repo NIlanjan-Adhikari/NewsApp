031d83b437bb363d714282d2f4743cc83336e989
NIlanjan-Adhikari <nilanjan.adhikari.phe17@itbhu.ac.in>
2023-11-19 13:03:20 +0530

    loading spinner added

--- src/components/News.js
+++ src/components/News.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import NewsItem from "./NewsItem";
+import Spinner from "./Spinner";
 
 export class News extends Component {
   constructor() {
@@ -14,40 +15,45 @@ export class News extends Component {
 
   async componentDidMount() {
     let url =
-      "https://newsapi.org/v2/top-headlines?country=in&apiKey=2f50ab40cb524053bf86d0b1f9be9d71&page=1&pageSize=20";
+      `https://newsapi.org/v2/top-headlines?country=in&apiKey=2f50ab40cb524053bf86d0b1f9be9d71&page=1&pageSize=${this.props.pageSize}`;
+    this.setState({loading: true});
     let data = await fetch(url);
     let parsedData = await data.json();
     console.log(parsedData);
-    this.setState({ articles: parsedData.articles, totalResults: parsedData.totalResults });
+    this.setState({ articles: parsedData.articles, 
+      totalResults: parsedData.totalResults,
+      loading: false
+     });
   }
 
   handlePrevClick = async () => {
     console.log("Previous");
     let url =
-      `https://newsapi.org/v2/top-headlines?country=in&apiKey=2f50ab40cb524053bf86d0b1f9be9d71&page=${this.state.page-1}&pageSize=20`;
+      `https://newsapi.org/v2/top-headlines?country=in&apiKey=2f50ab40cb524053bf86d0b1f9be9d71&page=${this.state.page-1}&pageSize=${this.props.pageSize}`;
+    this.setState({loading: true});
     let data = await fetch(url);
     let parsedData = await data.json();
     console.log(parsedData);
     this.setState({
       page: this.state.page + 1, 
-      articles: parsedData.articles 
+      articles: parsedData.articles,
+      loading: false 
     });
   };
 
   handleNextClick = async () => {
     console.log("Next");
-    if(this.state.page + 1 > Math.ceil(this.state.totalResults/20)){
-
-    }
-    else{
+    if (!(this.state.page + 1 > Math.ceil(this.state.totalResults/this.props.pageSize))){
     let url =
-      `https://newsapi.org/v2/top-headlines?country=in&apiKey=2f50ab40cb524053bf86d0b1f9be9d71&page=${this.state.page+1}&pageSize=20`;
+      `https://newsapi.org/v2/top-headlines?country=in&apiKey=2f50ab40cb524053bf86d0b1f9be9d71&page=${this.state.page+1}&pageSize=${this.props.pageSize}`;
+    this.setState({loading: true});
     let data = await fetch(url);
     let parsedData = await data.json();
     console.log(parsedData);
     this.setState({
       page: this.state.page + 1, 
-      articles: parsedData.articles 
+      articles: parsedData.articles,
+      loading: false 
     });
   }
   };
@@ -55,9 +61,10 @@ export class News extends Component {
   render() {
     return (
       <div className="container my-3">
-        <h2>NewsMonkey - Top Headlines</h2>
+        <h1 className="text-center">NewsMonkey - Top Headlines</h1>
+        {this.state.loading && <Spinner/>}
         <div className="row">
-          {this.state.articles.map((element) => {
+          {!this.state.loading && this.state.articles.map((element) => {
             return (
               <div className="col-md-4" key={element.url}>
                 <NewsItem
@@ -83,6 +90,7 @@ export class News extends Component {
             type="button"
             className="btn btn-dark"
             onClick={this.handleNextClick}
+            disabled={this.state.page + 1 > Math.ceil(this.state.totalResults/this.props.pageSize)}
           >
             Next &rarr;
           </button>
